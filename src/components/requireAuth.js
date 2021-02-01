a763991c6ce4d3ac6c1b20469ac8f664f138e3e0
Madalin Antonoiu <antonoiumadalin@gmail.com>
2021-02-01 17:30:22 +0000

    cheers netlify

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -3,6 +3,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+// eslint-disable-next-line
 export default (ChildComponent) => {
   class ComposedComponent extends Component {
     // Our component just got rendered
