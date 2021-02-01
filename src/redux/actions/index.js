409979869a4fd7f882c8caeb78ed8688161e4f1e
Madalin Antonoiu <antonoiumadalin@gmail.com>
2021-02-01 15:27:39 +0000

    fggfdg

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -4,7 +4,5 @@ import axios from "axios";
 //Returns an arrow function containing dispatch function, then we can invoke it
 export const signup = (formProps) => (dispatch) => {
   const url = process.env.REACT_APP_API_BACKEND_URL + "/signup";
-  console.log(formProps, url);
   axios.post(url, formProps);
-  // formProps is { email: email, password: password}
 };
