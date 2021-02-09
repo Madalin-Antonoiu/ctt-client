d74ccf2ab15d32bafb30501315a4325efce8311d
Madalin Antonoiu <antonoiumadalin@gmail.com>
2021-02-09 17:19:22 +0000

    mda

--- src/views/Welcome/Welcome.js
+++ src/views/Welcome/Welcome.js
@@ -1,5 +1,33 @@
 import React from "react";
 
 export const Welcome = () => {
-  return <h3>Welcome! Please sign up or sign in</h3>;
+  return <div>
+    <h3>Welcome! Please sign up or sign in</h3>
+
+    {/* <h3>Preview</h3> */}
+
+    {/* <table>
+      <tr>
+        <th>Coin</th>
+        <th>Now</th>
+        <th>1m ago</th>
+        <th>3m ago</th>
+        <th>5m ago</th>
+        <th>15m ago</th>
+        <th>30m ago</th>
+        <th>45m ago</th>
+        <th>1h ago</th>
+      </tr>
+      <tr>
+        <td>DOGEUSDT</td>
+        <td>0.018000</td>
+        <td>0.017000 (+ 5%)</td>
+        <td>0.016000 (+ 10%)</td>
+        <td>0.015000 (+ 15%)</td>
+      </tr>
+    </table>
+  */}
+
+
+  </div>
 };
